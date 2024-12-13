10d76fb929174dc7dbc70b04d3130674ac0cf1a5
jokokendil00 <jokokendil4410@gmail.com>
2024-12-13 23:28:52 +0700

    tes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('./'));
 
 var routes = require("./api/routes");
 routes(app);
